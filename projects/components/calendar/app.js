ac1509bcb4298cef56f5b952082ad6ecd4cc14cb
mehmetcanaygun <mehmetcanaygun@gmail.com>
2021-10-19 17:21:04 +0300

    Update Calendar project

--- projects/components/calendar/app.js
+++ projects/components/calendar/app.js
@@ -6,7 +6,8 @@ const currentMonthYear = document.querySelector(".current-month-year");
 
 // Constants & Variables
 let date = new Date();
-const months = [
+const TOTAL_DAYS_VISIBLE = 42;
+const MONTHS = [
   "January",
   "February",
   "March",
@@ -23,23 +24,24 @@ const months = [
 
 // Create calendar with the given date object
 function createCalendar(date) {
-  // Find the amount of days from the last, current and next month to be shown on the calendar
+  // Find the amount of days from the last, current and next month to show on the calendar
   const prev = getLastDate(date.getFullYear(), date.getMonth(), true);
   const curr = getLastDate(date.getFullYear(), date.getMonth() + 1);
-  const next = 42 - (prev.days + curr);
+  const next = TOTAL_DAYS_VISIBLE - (prev.days + curr);
 
-  // Clear date list first
+  // Firstly, clear the date list
   dateList.innerHTML = "";
 
-  // Fill prev days
+  // Fill previous days on the calendar
   for (let i = prev.date - prev.days + 1; i <= prev.date; i++) {
     dateList.innerHTML += `
       <li class="date">${i}</li>
     `;
   }
 
-  // Fill current days
+  // Fill current days on the calendar
   for (let i = 1; i <= curr; i++) {
+    // Check if the day is today
     if (date.getDate() === i) {
       dateList.innerHTML += `
         <li class="date current today">${i}</li>
@@ -51,16 +53,16 @@ function createCalendar(date) {
     }
   }
 
-  // Fill next days
+  // Fill next days on the calendar
   for (let i = 1; i <= next; i++) {
     dateList.innerHTML += `
       <li class="date">${i}</li>
     `;
   }
 
-  // Fill current month & year text
+  // Update current month & year
   currentMonthYear.innerText = `${
-    months[date.getMonth()]
+    MONTHS[date.getMonth()]
   }, ${date.getFullYear()}`;
 }
 
@@ -78,18 +80,17 @@ function nextMonth() {
   createCalendar(date);
 }
 
-// Get last date of the previous month
+// Helper function - Get last date of the previous month
 function getLastDate(year, month, withDay = false) {
-  if (withDay)
+  if (withDay) {
     return {
       date: new Date(year, month, 0).getDate(),
       days: new Date(year, month, 0).getDay(),
     };
+  }
 
   return new Date(year, month, 0).getDate();
 }
 
 // Event Listeners
-document.addEventListener("DOMContentLoaded", () =>
-  createCalendar(new Date(date))
-);
+document.addEventListener("DOMContentLoaded", () => createCalendar(date));
